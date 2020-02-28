4b6d684b0225690f2ac6b2c9cc51f5ab64c36139
Patrick Kille <pr.kille@gmail.com>
2020-02-28 11:18:58 -0800

    refactor Main to call new Order and Total functions.

--- Program.cs
+++ Program.cs
@@ -10,6 +10,12 @@ namespace PierresBakery
     public static void Main()
     {
       Console.WriteLine("Welcome to Pierre's Bakery. Where we're more Canadian than French. Sorry!\nWe're currently running two specials:\nBuy 2 $5 loaves of bread get 1 free!\nFor Pastry Buy 1 for $2 or stock up with 3 for $5!");
+      Order();
+      Console.WriteLine(Total());
+    }
+
+    public static void Order()
+    {
       Console.Write("How many loaves of bread would you like: ");
       int breadLoaves = int.Parse(Console.ReadLine());
       breadOrder.Loaves = breadLoaves;
@@ -18,7 +24,11 @@ namespace PierresBakery
       int pastries = int.Parse(Console.ReadLine());
       pastryOrder.Pastries = pastries;
       pastryOrder.PastryDeal();
-      Console.WriteLine($"Your order is {breadOrder.Loaves} loaves of bread and {pastryOrder.Pastries} pastries.\nYour total is ${pastryOrder.Price + breadOrder.Price}");
+    }
+
+    public static string Total()
+    {
+      return $"Your order is {breadOrder.Loaves} loaves of bread and {pastryOrder.Pastries} pastries.\nYour total is ${pastryOrder.Price + breadOrder.Price}";
     }
   }
 }
