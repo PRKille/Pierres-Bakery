ddfa9e285e50621a6225b4b8dcc193c585a97e5f
Patrick Kille <pr.kille@gmail.com>
2020-02-28 09:38:38 -0800

    add Pastry class, constructor, and price function.

---
+++ Models/Pastry.cs
@@ -0,0 +1,19 @@
+namespace PierresBakery.Models
+{
+  public class Pastry
+  {
+    public int Pastries { get; set; }
+    public int Price { get; set; }
+
+    public Pastry()
+    {
+      Pastries = 0;
+      Price = 0;
+    }
+
+    public void PastryDeal()
+    {
+      Price = (Pastries/3*5)+(Pastries%3*2);
+    }
+  }
+}
